d3da3e6a8c85dcdfcfd49bf6c39df24d88adb5b1
Thomas Melo <thomas.melo@terra;com.br>
2023-07-21 14:36:11 -0300

    Show Produtos

--- resources/views/produto/show.blade.php
+++ resources/views/produto/show.blade.php
@@ -1,35 +1,47 @@
 @extends('layouts.base')
 @section('content')
 
-<h1> Cargo:{{ $cargo->cargo }} </h1>
-<h2> Relação de Usuários com esse cargo</h2>
+<h1> Produto:{{ $produto->nome }} </h1>
+<h2> Tipo: {{ $produto->tipo->tipo }} </h2>
+<p> Descrição: {!! nl2br($produto->descricao) !!}</p>
+@if ($produto->observacoes)
+    <p class="alert alert-info">
+        {!! nl2br($produto->observacoes) !!}
+    </p>
+@endif
 
 <table class="table table-striped table-hover">
     <thead>
         <tr>
             <th class="col-2">Ações</th>
-            <th>Nome</th>
+            <th>Tamanho</th>
+            <th>Preço</th>
+            <th>Obs.:</th>
         </tr>
     </thead>
     <tbody>
-        @forelse ($cargo->usuarios()->get() as $usuario)
+        @forelse ($produto->tamanhos()->get() as $item)
          <tr>
             <td>
+                {{-- editar --}}
                 <a class="btn btn-primary" href="#">
                     <i class="fa-solid fa-pen-to-square"></i>
                 </a>
-                <a class="btn btn-success" href="#">
-                    <i class="fa-solid fa-eye"></i>
-                </a>
             </td>
             <td>
-                {{ $usuario->nome}}
+                {{ $item->tamanhos->tamanho->tamanho}}
+            </td>
+            <td>
+                {{ $item->preco }}
+            </td>
+            <td>
+                {!! nl2br($item->observacoes) !!}
             </td>
         </tr>
         @empty
         <tr>
-            <td colspan="2">
-                Nenhum usuário com esse cargo
+            <td colspan="4">
+                Nenhum tamanho definido para esse produto
             </td>
         </tr>
         @endforelse
